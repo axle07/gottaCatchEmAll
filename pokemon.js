4ae341c1c67da5e0dc3230c1d5a2d5bde44da656
alex <meetalexjohnson@gmail.com>
2022-03-20 21:32:36 -0700

    Add comments

--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,14 @@
 const mainDiv = document.getElementById('pokemons')
 
+https://developer.mozilla.org/en-US/docs/Web/API/fetch
 const fetchPokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=20/')
 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
 fetchPokemon.then(res => {
+  https://developer.mozilla.org/en-US/docs/Web/API/Response/json
   res.json().then(pokemons => {
     pokemons.results.forEach(pokemon => {
-    console.log(pokemon)
       const link = document.createElement('a')
       link.innerText = pokemon.name
       link.href = pokemon.url
