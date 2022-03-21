e47d902dd04e829b1fbe5015fddc4ef8d4b2ca17
alex <meetalexjohnson@gmail.com>
2022-03-20 21:39:47 -0700

    Fixing comment syntax

--- pokemon.js
+++ pokemon.js
@@ -1,12 +1,12 @@
 const mainDiv = document.getElementById('pokemons')
 
-https://developer.mozilla.org/en-US/docs/Web/API/fetch
+// https://developer.mozilla.org/en-US/docs/Web/API/fetch
 const fetchPokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=20/')
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
 fetchPokemon.then(res => {
-  https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+  // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
   res.json().then(pokemons => {
     pokemons.results.forEach(pokemon => {
       const link = document.createElement('a')
